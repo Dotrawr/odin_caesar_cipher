7ae53ee36d6e4f1ff31d1995df468ff39adf9791
Lee Johnson <Contact@leejohnson.dev>
2022-05-24 19:20:19 +0100

    Implemented Ruby Style Guide

--- caesar.rb
+++ caesar.rb
@@ -1,20 +1,18 @@
+# frozen_string_literal: true
+
 def caesar_cipher(plaintext, shift_factor)
   cipher_text = ''
   plaintext.each_byte do |byte|
     cipher_text += shift_char(byte, shift_factor)
   end
-  return cipher_text
+  cipher_text
 end
 
 def shift_char(byte, shift_factor)
-  if byte.between?(65, 90) # A-Z
-    return (65 + (byte - 65 + shift_factor) % 26).chr
-  end
-  if byte.between?(97, 122) # a-z
-    return (97 + (byte - 97 + shift_factor) % 26).chr
-  end
+  return (65 + (byte - 65 + shift_factor) % 26).chr if byte.between?(65, 90) # A-Z
+  return (97 + (byte - 97 + shift_factor) % 26).chr if byte.between?(97, 122) # a-z
 
-  return byte.chr
+  byte.chr
 end
 
 puts('Please enter a string to encode: ')
